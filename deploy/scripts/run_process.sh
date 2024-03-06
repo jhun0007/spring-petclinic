683b649f48438974ec57044cdfa19734afaf5c95
baxk1503 <134983748+baxk1503@users.noreply.github.com>
2024-03-06 15:36:49 +0900

    Update run_process.sh

--- deploy/scripts/run_process.sh
+++ deploy/scripts/run_process.sh
@@ -2,7 +2,7 @@
 
 REGION="ap-northeast-2"
 ECR_REPOSITORY="257307634175.dkr.ecr.ap-northeast-2.amazonaws.com"
-ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/aws06-spring-petclinic"
+ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/aws00-spring-petclinic"
 ECR_DOCKER_TAG="latest"
 
 aws ecr get-login-password --region ${REGION} \
@@ -10,4 +10,6 @@ aws ecr get-login-password --region ${REGION} \
 
 export IMAGE=${ECR_DOCKER_IMAGE};
 export TAG=${ECR_DOCKER_TAG};
-docker-compose -f /home/ubuntu/deploy/docker-compose.yml up -d --build;
+# ubuntu directory로 이동
+cd /home/ubuntu/deploy
+docker-compose up -d --build;
